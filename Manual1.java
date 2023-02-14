12899a79b01bf48a17909ede4076944f67170a70
twinkal patel <twinklepatel491@gmail.com>
2023-02-14 22:07:48 +0000

    manual1 file added

---
+++ Manual1.java
@@ -0,0 +1,23 @@
+package manualtopic;
+
+//the first package name is manual and rename it as manual topic
+public class Manual1{
+    /* create class as ManualTopic and rename it as manual1 */
+    public static void main(String[] args) {
+        System.out.println("Print 10 manual topic name");
+        System.out.println("What is namual testing?");
+        System.out.println("Regration");
+        System.out.println("What is retesting?");
+        System.out.println("What is bug");
+        System.out.println("What is buglife cycle?");
+        System.out.println("What is white box testing?");
+        System.out.println("What is black box testing?");
+        System.out.println("Software development models");
+        System.out.println("tetsing techniques");
+        System.out.println("SDLC process");
+
+    }
+
+}
+
+
