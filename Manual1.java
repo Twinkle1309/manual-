edbfbb616869019f379ab76b6db8102c5b234d46
twinkal patel <twinklepatel491@gmail.com>
2023-02-15 21:48:22 +0000

    change

--- Manual1.java
+++ Manual1.java
@@ -5,17 +5,17 @@ public class Manual1{
     /* create class as ManualTopic and rename it as manual1 */
     public static void main(String[] args) {
         System.out.println("Print 10 manual topic name");
-        System.out.println("What is namual testing?");
-        System.out.println("Regration");
+        System.out.println("What is manual testing?");
+        System.out.println("priority");
         System.out.println("What is retesting?");
         System.out.println("What is bug");
-        System.out.println("What is buglife cycle?");
+        System.out.println("What is bug-life cycle?");
         System.out.println("What is white box testing?");
         System.out.println("What is black box testing?");
         System.out.println("Software development models");
-        System.out.println("tetsing techniques");
+        System.out.println("testing techniques");
         System.out.println("SDLC process");
-
+        System.out.println("Finish");
     }
 
 }
